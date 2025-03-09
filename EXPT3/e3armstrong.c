eca9bc846c8deaec34f29aee6d595ac4720c89ea
RageCoder2006 <rishitbaitule@outlook.com>
2025-03-09 16:59:06 +0530

    implemented while and do-while loops instead of for loops

--- EXPT3/e3armstrong.c
+++ EXPT3/e3armstrong.c
@@ -2,6 +2,21 @@
 #include <stdio.h>
 #include <math.h>
 
+int main() {
+    int number = 100;
+    while (number <= 500) {
+        int dig1 = number % 10;
+        int dig2 = (number % 100) / 10;
+        int dig3 = number / 100;
+        int cond = pow(dig1, 3) + pow(dig2, 3) + pow(dig3, 3);
+        if (cond == number) {
+            printf("%d is an armstrong number\n", number);
+        }
+        number++;
+    }
+}
+
+/* using for loop
 int main(){
   int number;
   for(number = 100; number <= 500; number++){
@@ -14,6 +29,6 @@ int main(){
     }
     else {
     }
-  }
-
 }
+
+}*/
--- EXPT3/e3sum50N.c
+++ EXPT3/e3sum50N.c
@@ -1,11 +1,20 @@
 // sum of first 50 natural numbers
 #include <stdio.h>
 
-int main(){
-  int num,sum;
-  sum = 0;
-  for (num = 1; num <= 50; num++){
+int main() {
+    int num, sum;
+    num = 1;
+    sum = 0;
+    do {
+        sum += num;
+        num++;
+    } while (num <= 50);
+    printf("The sum of the first 50 natural numbers is: %d\n", sum);
+}
+
+/* using for loop
+for (num = 1; num <= 50; num++){
     sum = sum + num;
   }
   printf("The sum of the first 50 natural numbers is: %d\n", sum);
-}
+ */
