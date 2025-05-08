0d9ba7b7b037f13a6b85b484f588152c3125dca4
RageCoder2006 <rishitbaitule@outlook.com>
2025-05-08 18:39:09 +0530

    some random problems

--- xQB/acc.cpp
+++ xQB/acc.cpp
@@ -3,29 +3,29 @@ using namespace std;
 
 class Account {
 private:
-    int accno;
-    string name;
+  int accno;
+  string name;
 
 public:
-    char acctype; //s-> savings, c-> current
-    float balance;
+  char acctype; //s-> savings, c-> current
+  float balance;
 
-    Account(int accno, string name, char acctype, float balance) {
-        this->accno = accno;
-        this->name = name;
-        this->acctype = acctype;
-        this->balance = balance;
-    }
+  Account(int accno, string name, char acctype, float balance) {
+    this->accno = accno;
+    this->name = name;
+    this->acctype = acctype;
+    this->balance = balance;
+  }
 
-    void display() {
-        cout << "AccountNumber: " << accno << endl;
-        cout << "Account: " << name << endl;
-        cout << "Balance: " << balance << endl;
-        cout << "AccountType: " << acctype << endl;
-    }
+  void display() {
+    cout << "AccountNumber: " << accno << endl;
+    cout << "Account: " << name << endl;
+    cout << "Balance: " << balance << endl;
+    cout << "AccountType: " << acctype << endl;
+  }
 };
 
 int main() {
-    Account acc(123, "xyz", 'c', 123.21);
-    acc.display();
+  Account acc(123, "xyz", 'c', 123.21);
+  acc.display();
 }
