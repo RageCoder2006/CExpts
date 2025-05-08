3e15de54ad26d651629d3cf24f9bba53b18cdc24
RageCoder2006 <rishitbaitule@outlook.com>
2025-05-08 19:27:46 +0530

    some random problems

--- xSearchingAlgorithms/gcd.c
+++ xSearchingAlgorithms/gcd.c
@@ -2,18 +2,19 @@
 
 int gcd(int a, int b);
 
-int main(){
-  int n1, n2, res;
-  printf("Enter number 1 and 2: ");
-  scanf("%d %d", &n1, &n2);
-  res = gcd(n1, n2);
-  printf("GCD of %d and %d is %d", n1, n2, res);
-  return 0;
+int main() {
+    int n1, n2, res;
+    printf("Enter number 1 and 2: ");
+    scanf("%d %d", &n1, &n2);
+    res = gcd(n1, n2);
+    printf("GCD of %d and %d is %d", n1, n2, res);
+    return 0;
 }
+
 // Euclidean Algorithm to find GCD
 int gcd(int a, int b) {
-  if (b == 0) {
-    return a;
-  }
-  return gcd(b, a % b);
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
 }
